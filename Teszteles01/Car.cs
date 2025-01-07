d895a86e96993f0fce02df9aee4c1ba0b7a311f6
Szőllősy István <szollosyi@kkszki.hu>
2025-01-07 11:50:37 +0100

    Teszteles01

--- Teszteles01/Car.cs
+++ Teszteles01/Car.cs
@@ -6,18 +6,24 @@ using System.Threading.Tasks;
 
 namespace Teszteles01
 {
-    internal class Car
+    public class Car
     {
         public string Brand { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
-        public int Tax { get; set; }
-        public Car (string brand, string model, int year, int tax)
+        //Az adó mértéke 10 évnél fiatalabb autó esetén 20000 Ft, 10 és 30 év között 10000 Ft, 30 év fölött 0 Ft
+        public int Tax { get {if (DateTime.Today.Year - Year < 10)
+                                return 20000;
+                            else if (DateTime.Today.Year - Year < 30)
+                                return 10000;
+                            else
+                                return 0;
+                    } }
+        public Car (string brand, string model, int year)
         {
             Brand = brand;
             Model = model;
             Year = year;
-            Tax = tax;
         }
         public override string ToString()
         {
