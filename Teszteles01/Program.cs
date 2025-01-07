084ea3d5098b1d9a404f2b16f489344a7e77803b
Szőllősy István <szollosyi@kkszki.hu>
2025-01-07 11:30:16 +0100

    Add project files.

---
+++ Teszteles01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszteles01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot:");
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine(Negyzet(szam));
+            Console.ReadLine();
+        }
+
+        public static int Negyzet(int szam)
+        {
+            return szam + szam;
+        }
+    }
+}
---
+++ Teszteles01Tests/ProgramTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Teszteles01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszteles01.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void NegyzetTest1()
+        {
+            //Arrange
+            int szam = 2;
+            int expected = 4;
+            //Act
+            int actual = Program.Negyzet(szam);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NegyzetTest2()
+        {
+            //Arrange
+            int szam = 3;
+            int expected = 9;
+            //Act
+            int actual = Program.Negyzet(szam);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NegyzetTest3()
+        {
+            //Arrange
+            int szam = 3;
+            int expected = 6;
+            //Act
+            int actual = Program.Negyzet(szam);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NegyzetTest4()
+        {
+            //Arrange
+            int szam = -3;
+            int expected = 9;
+            //Act
+            int actual = Program.Negyzet(szam);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
